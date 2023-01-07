c2376385964566ac8f763314d719bd99e4c531ff
DanielGu23 <daniel.gu23@ycdsbk12.ca>
2023-01-06 21:28:35 -0500

    Added Header

---
+++ src/cpt/Main.java
@@ -0,0 +1,41 @@
+package cpt;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+* The main page of the covid-19 data explorer
+*
+* @author: D.Gu
+*/
+
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello JavaFX!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello JavaFX'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello JavaFX!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
