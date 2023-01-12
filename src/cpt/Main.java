cf750a623cc4d03d8fc9da4b6c2f5e98420908c6
DanielGu23 <daniel.gu23@ycdsbk12.ca>
2023-01-11 20:36:19 -0500

    Added main page

--- src/cpt/Main.java
+++ src/cpt/Main.java
@@ -1,42 +1,46 @@
 package cpt;
 
+import java.io.*;
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
 
 /**
 * The main page of the covid-19 data explorer
 *
-* @author: D.Gu
+* @author: Daniel Gu
 */
 
+	public class Main extends Application {
+		private DataSet dataSet;
+		private BorderPane border;
+		
+		public static Scene mainScene;
+	    
+	    /**
+	     * Main method that launches Javafx
+	     * @param - args, defult main method parameter
+	     */
+	    public static void main(String[] args) throws IOException {
+	        launch(args);
+	    }
 
-public class Main extends Application {
-    public static void main(String[] args) {
-        DataSet dataSet = new DataSet();
-        launch(args);
-    }
-    
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello JavaFX!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello JavaFX'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello JavaFX!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
-    }
+	    
+	    /**
+	     * A method that creates the home page
+	     * 
+	     * @param - primaryStage,  The main stage where all the scenes are shown
+	     */
+	    @Override
+	    public void start(Stage primaryStage) throws IOException {	    	
+	    	primaryStage.setTitle("COVID-19 Data Explorer");	  
+	    	dataSet = new DataSet();
+            border = new BorderPane();            	    	
+	        // Set the main scene
+	        mainScene = new Scene(border, 954, 600);
+	        primaryStage.setScene(mainScene);
+	        primaryStage.show();
+	    }
 }
